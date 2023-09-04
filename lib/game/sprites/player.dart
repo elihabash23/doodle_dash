d9308aee51d7a9692330748ba87052144f0b9b61
Elias Habash <ehabash@MacBook-Pro.local>
2023-09-03 22:41:37 -0700

    started the player class

--- lib/game/sprites/player.dart
+++ lib/game/sprites/player.dart
@@ -15,4 +15,35 @@ class Player extends SpriteGroupComponent<PlayerState> with HasGameRef<DoodleDas
     Character character;
     double jumpSpeed;
 
+    @override
+    Future<void> onLoad() async {
+      await super.onLoad();
+
+      await add(CircleHitbox());
+
+      await _loadCharacterSprites();
+      current = PlayerState.center;
+    }
+    
+     Future<void> _loadCharacterSprites() async {
+      // Load and configure Sprite assets
+      final left = await gameRef.loadSprite('game/${character.name}_left.png');
+      final right = await gameRef.loadSprite('game/${character.name}_right.png');
+      final center = await gameRef.loadSprite('game/${character.name}_center.png');
+      final rocket = await gameRef.loadSprite('game/rocket_4.png');
+      final nooglerCenter = await gameRef.loadSprite('game/${character.name}_hat_center.png');      
+      final nooglerLeft = await gameRef.loadSprite('game/${character.name}_hat_left.png');
+      final nooglerRight = await gameRef.loadSprite('game/${character.name}_hat_right.png');
+
+      sprites = <PlayerState, Sprite>{
+        PlayerState.left: left,
+        PlayerState.right: right,
+        PlayerState.center: center,
+        PlayerState.rocket: rocket,
+        PlayerState.nooglerLeft: nooglerLeft,
+        PlayerState.nooglerRight: nooglerRight,
+        PlayerState.nooglerCenter: nooglerCenter,
+      };
+     }
+
 }
